ef136f4e775e589cd13ed29af366b310a4d4d9c2
crypticCaroline <rebecca.kelsall@sky.com>
2021-02-17 16:25:10 +0000

    Added task input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip({delay: 50});
-  });
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done:"select"
+        }
+    });
+});    
