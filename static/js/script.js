9ef04c94cb1e9055a3cd338c224869857790838e
crypticCaroline <rebecca.kelsall@sky.com>
2021-02-18 09:57:11 +0000

    Added submit function

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip({delay: 50});
+    $('select').formSelect();
     $('.datepicker').datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
